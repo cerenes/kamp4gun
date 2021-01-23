5127a380ed3069d1c258a3e45478fd298d3f2bb8
ceren esirgenci <ceren_esirgenci@hotmail.com>
2021-01-23 18:22:17 +0300

    Proje dosyası ekle.

---
+++ kamp4gun/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kamp4gun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
