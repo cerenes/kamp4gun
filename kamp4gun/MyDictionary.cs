acb8d485a2ecbe282cd7ddd829a57a006ecce720
ceren esirgenci <ceren_esirgenci@hotmail.com>
2021-01-23 19:10:01 +0300

    eşit

---
+++ kamp4gun/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kamp4gun
+{
+    class MyDictonary<TValue, TKey>
+    {
+        TValue[] values;
+        TKey[] keys;
+
+        public MyDictonary()
+        {
+            values = new TValue[0];
+            keys = new TKey[0];
+        }
+
+        public void Add(TValue val, TKey key)
+        {
+            TValue[] tempVals = values;
+            TKey[] tempKeys = keys;
+
+
+            values = new TValue[values.Length + 1];
+            keys = new TKey[keys.Length + 1];
+
+            for (int i = 0; i < tempVals.Length; i++)
+            {
+                values[i] = tempVals[i];
+                keys[i] = tempKeys[i];
+
+
+            }
+            
+        }
+        public int Count
+        {
+            get { return values.Length; }
+        }
+    }
+}
+
+
+
+
--- kamp4gun/Program.cs
+++ kamp4gun/Program.cs
@@ -10,6 +10,13 @@ namespace kamp4gun
     {
         static void Main(string[] args)
         {
+            MyDictonary<string, int> dictonary = new MyDictonary<string, int>();
+
+            dictonary.Add("Ceren", 1);
+            dictonary.Add("Esra", 2);
+            dictonary.Add("Melike",3);
+            Console.WriteLine(dictonary.Count);
+            Console.ReadKey();
         }
     }
 }
